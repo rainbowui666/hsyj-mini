5579c4ac3559da8f8b662aac393abf720daa6f81
Danni Xing <630082237@qq.com>
2019-03-24 14:13:31 +0800

    update

---
+++ src/pages/map/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="container">地图页面</view>
+</template>
+
+<script>
+export default {
+  computed: {},
+  data () {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+page {
+  overflow-x: hidden;
+}
+</style>
