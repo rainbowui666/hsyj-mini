8a88ec852dd3a4a8fcf718be5f06b9b6adfe199f
Xin <md102@MD102deMacBook-Pro.local>
2019-03-30 15:32:58 +0800

    add integral

--- src/pages/center/index.vue
+++ src/pages/center/index.vue
@@ -1,8 +1,9 @@
 <template >
     <view class="container">
         <wux-cell-group class="mySelf_group">
-            <wux-cell title="我的签到" is-link extra="2次"></wux-cell>
-            <wux-cell title="参加过的活动" is-link extra="12次"></wux-cell>
+            <wux-cell title="我的签到" is-link extra="2次"  @click="gotoSignin"></wux-cell>
+            <wux-cell title="我的留言" is-link extra="2次"  @click="gotoMessage"></wux-cell>
+            <wux-cell title="参加过的活动" is-link extra="12次"  @click="gotoActicity"></wux-cell>
             <wux-cell title="我的积分" is-link extra="10分" @click="gotoIntegral"></wux-cell>
         </wux-cell-group>
     </view>
@@ -25,11 +26,17 @@ export default {
     buttonClicked () {
       console.log('1')
     },
+    gotoSignin () {
+      wx.navigateTo({url: '/pages/center/signin/main'})
+    },
+    gotoMessage () {
+      wx.navigateTo({url: '/pages/center/message/main'})
+    },
+    gotoActicity () {
+      wx.navigateTo({url: '/pages/center/myactivity/main'})
+    },
     gotoIntegral () {
-      wx.removeStorageSync('userInfo');
-      wx.reLaunch({
-        url: '/pages/center/login/main'
-      });
+      wx.navigateTo({url: '/pages/center/integral/main'})
     }
   }
 };
---
+++ src/pages/center/integral/index.vue
@@ -0,0 +1,37 @@
+<template >
+    <view class="container">
+        <wux-cell-group class="myintegral_group">
+            <wux-cell title="我的签到" extra="10分"></wux-cell>
+            <wux-cell title="我的留言" extra="8分"></wux-cell>
+            <wux-cell title="参加过的活动" extra="10分"></wux-cell>
+        </wux-cell-group>
+    </view>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped>
+page {
+  height: 100%;
+  width: 100%;
+}
+.container {
+  background: #ffffff;
+  height: auto;
+  overflow: hidden;
+  width: 100%;
+  position: relative;
+}
+.myintegral_group{
+  width: 100%;
+}
+</style>
