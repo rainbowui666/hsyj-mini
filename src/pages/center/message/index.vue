10eaff5e39757b18680a856c2c93d52441ff33a5
Xin <md102@MD102deMacBook-Pro.local>
2019-04-01 23:40:18 +0800

    add tab

--- src/pages/center/message/index.vue
+++ src/pages/center/message/index.vue
@@ -1,6 +1,17 @@
 <template >
   <view class="message-page">
-    <head-message :valueData="valueData"> </head-message>
+      <wux-tabs controlled :current="current" @change="onChange">
+        <wux-tab key="message_tab1">
+            <wux-badge count="3">累计留言</wux-badge>
+        </wux-tab>
+        <wux-tab key="message_tab2">
+            <wux-badge count="1024">审核通过</wux-badge>
+        </wux-tab>
+        <wux-tab key="message_tab3">
+            <wux-badge count="4">未审核</wux-badge>
+        </wux-tab>
+      </wux-tabs>
+    <!-- <head-message :valueData="valueData"> </head-message> -->
     <navigation-card :data="activityList" :iconClick="iconClick"/>
   </view>
 </template>
@@ -15,6 +26,7 @@ export default {
   },
   data () {
     return {
+      current: 'message_tab1',
       valueData: '累计留言：50条  审核通过：40条  未审核：5条',
       activityList: [
         {
@@ -65,6 +77,10 @@ export default {
     iconClick (item) {
       console.log('item', item)
       wx.navigateTo({ url: 'activityDetail/main?name=' + item.activityName })
+    },
+    onChange (e) {
+      console.log('onChange', e)
+      this.current = e.target.key;
     }
   }
 };
