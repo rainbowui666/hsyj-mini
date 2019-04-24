c238b055cb5c48019aeb2a1c2551696c79446e85
Danni <630082237@qq.com>
2019-04-24 23:50:51 +0800

    更新首页

--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -1,277 +1,120 @@
-<template>
-  <view class='container'>
-    <map
-      id='myMap'
-      :longitude='centerX'
-      :latitude='centerY'
-      :scale='scale'
-      @controltap='controltap'
-      :markers='markers'
-      @markertap='markertap'
-      @regionchange='regionchange'
-      show-location
-      :style='winStyle'
-    ></map>
-    <cover-image class='search_bar' src='/static/images/search_bar.png' @click='gotoNavigation'></cover-image>
-    <cover-image class='liuyan' src='/static/images/edit.png' @click='messageDialog'></cover-image>
-    <div v-show='showDetail' class='all-bg'>
-      <cover-view class='dw-bg' @click='hidentap'></cover-view>
-      <cover-image class='bg' src='/static/images/liuyan_bg.png'></cover-image>
-      <cover-view class='dw-text-body'>
-        <textarea placeholder="请留下你的想法" name="textarea" />
-      </cover-view>
-      <cover-view class='dw-text-cancle'>
-        <button class="button_cancle" @click='cancleDialog'>撤销</button>
-      </cover-view>
-      <cover-view class='dw-text-confirm'>
-        <button class="button_confirm" @click='confirmDialog'>发布</button>
-      </cover-view>
-    </div>
+<template >
+  <view class="activity-page">
+    <swiper class="swiper" :indicator-dots="true" :autoplay="true" interval="3000" duration="800">
+      <block v-for="(item, index) in movies" :key="index">
+        <swiper-item>
+          <image :src="item.url" class="slide-image"/>
+        </swiper-item>
+      </block>
+    </swiper>
+    <view class="scenic-spot-recommendation-group">
+      <view class="scenic-spot-recommendation-text">景点推荐</view>
+      <scroll-view scroll-x="true" class="scenic-spot-recommendation">
+        <view class="scenic-spot-recommendation-view">
+          <view
+            scroll-x="true"
+            v-for="(item, index) in imgList"
+            :key="index"
+            class="scenic-spot-recommendation-view-inner"
+          >
+            <image :src="item.url" class="scenic-spot-recommendation-view-inner-image"/>
+          </view>
+        </view>
+      </scroll-view>
+    </view>
   </view>
 </template>
 
 <script>
 export default {
-  computed: {},
   data () {
     return {
-      service: {},
-      centerX: 121.475186,
-      centerY: 31.228725,
-      scale: 11,
-      markers: [],
-      showDetail: false,
-      winStyle: 'width: 100%; height: 400px;',
-      testList:
-      [
+      movies: [
         {
-          'id': 1,
-          'user_id': 5582,
-          'longitude': 121.506043,
-          'latitude': 31.155732,
-          'province': 'sh',
-          'location': '上海市浦东新区上南路3521号'
+          url: '../../static/images/fudan1.jpeg'
         },
         {
-          'id': 2,
-          'user_id': 7318,
-          'longitude': 121.25058,
-          'latitude': 31.27184,
-          'province': 'sh',
-          'location': '上海市嘉定区联西路11号'
+          url: '../../static/images/fudan2.jpeg'
         },
         {
-          'id': 3,
-          'user_id': 7318,
-          'longitude': 121.565781,
-          'latitude': 31.159934,
-          'province': 'sh',
-          'location': '上海市嘉定区联西路11号'
+          url: '../../static/images/fudan1.jpeg'
         },
         {
-          'id': 4,
-          'user_id': 7318,
-          'longitude': 121.451797,
-          'latitude': 31.200468,
-          'province': 'sh',
-          'location': '上海市嘉定区联西路11号'
+          url: '../../static/images/fudan2.jpeg'
+        }
+      ],
+      imgList: [
+        {
+          url: '../../static/images/fudan1.jpeg'
         },
         {
-          'id': 5,
-          'user_id': 7318,
-          'longitude': 121.348801,
-          'latitude': 31.154058,
-          'province': 'sh',
-          'location': '上海市嘉定区联西路11号'
+          url: '../../static/images/fudan2.jpeg'
+        },
+        {
+          url: '../../static/images/fudan1.jpeg'
+        },
+        {
+          url: '../../static/images/fudan2.jpeg'
+        },
+        {
+          url: '../../static/images/fudan1.jpeg'
+        },
+        {
+          url: '../../static/images/fudan2.jpeg'
+        },
+        {
+          url: '../../static/images/fudan1.jpeg'
+        },
+        {
+          url: '../../static/images/fudan2.jpeg'
         }
       ]
     };
   },
-  mounted () {
-    const markers = [];
-    const schoolList = this.testList;
-    for (let item of schoolList) {
-      let marker = this.createMarker(item);
-      markers.push(marker);
-    }
-    this.markers = markers;
-  },
-
-  methods: {
-    createMarker (point) {
-      let latitude = point.latitude;
-      let longitude = point.longitude;
-      this.includePoints = {
-        latitude: latitude,
-        longitude: longitude
-      };
-      let marker = {
-        id: point.id || 0,
-        latitude: latitude,
-        longitude: longitude,
-        width: 36,
-        height: 36,
-        userId: point.user_id,
-        location: point.location,
-        iconPath:
-          'http://118.190.96.118/hsyj/fudan.jpg'
-      };
-      return marker;
-    },
-    messageDialog () {
-      this.showDetail = true;
-    },
-    cancleDialog () {
-      this.showDetail = false;
-    },
-    confirmDialog () {
-      this.showDetail = false;
-    },
-    gotoNavigation () {
-      wx.navigateTo({url: '/pages/map/navigation/main'})
-    },
-    controltap (e) {
-      this.moveToLocation();
-    },
-    markertap (e) {
-      // let maker = null;
-      // for (let item of this.markers) {
-      //   if (item.id === e.mp.markerId) {
-      //     maker = item;
-      //   }
-      // }
-      // this.service = maker;
-      wx.navigateTo({url: '/pages/map/schools/main'})
-    },
-    regionchange (e) {
-      if (e.type === 'end') {
-        this.scale = e.scale;
-      }
-    }
-  },
-  onReady: function (e) {
-    this.mapCtx = wx.createMapContext('myMap');
-  },
-  onLoad: function () {
-    wx.getLocation({
-      type: 'gcj02',
-      success: res => {
-        let latitude = res.latitude;
-        let longitude = res.longitude;
-        this.centerX = longitude;
-        this.centerY = latitude;
-        this.scale = 12;
-      }
-    });
-    wx.getSystemInfo({
-      success: res => {
-        let mapWidth = res.windowWidth;
-        let mapHeight = res.windowHeight;
-        this.winStyle = 'width:' + mapWidth + 'px;height:' + mapHeight + 'px;';
-      }
-    });
-  }
+  methods: {}
 };
 </script>
 
-<style scoped>
-page {
-  overflow-x: hidden;
+<style>
+.activity-page {
+  background-color: #f4f4f4;
 }
-.character-item {
-  margin-top: 50px;
-}
-.all-bg {
-  width: 90%;
-  left: 5%;
-}
-.dw_msg {
-  width: 30px;
-  height: 30px;
-  bottom: 5%;
-  position: absolute;
-  left: 8%;
-}
-.dw_call {
-  width: 70px;
-  height: 30px;
-  bottom: 3%;
-  position: absolute;
-  right: 8%;
-}
-.bg {
-  width: 90%;
-  height: 40%;
-  position: absolute;
-  left: auto;
-  right: auto;
-  bottom: 30%;
-}
-.dw-bg {
-  position: absolute;
+.activity-page .swiper {
   width: 100%;
+  height: 230px;
+}
+.activity-page .slide-image {
+  width: 100%;
+}
+.activity-page .common-card-content-icongroup .icon-group {
+  width: 95%;
+}
+.scenic-spot-recommendation-group{
+  height: 120px;
+  margin-top: 15px;
+  background-color: #fff;
+}
+.scenic-spot-recommendation {
+  display: flex;
+  flex-direction: row;
   height: 100%;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.6);
 }
-.dw-text-body {
-  width: 84%;
-  height: 20%;
-  top: 35%;
-  position: absolute;
-  font-size: 12px;
-  white-space: normal;
-  left: 8%;
-  color: #777;
-  line-height: 1.5;
-  border: 1px solid #333;
+.scenic-spot-recommendation-text {
+  margin-left: 10px;
 }
-.dw-text-cancle{
-  width: 25%;
-  height:15%;
-  top: 60%;
-  position: absolute;
-  font-size: 12px;
-  white-space: normal;
-  left: 15%;
-  color: #777;
-  line-height: 1.5;
+.scenic-spot-recommendation-view {
+  height: 85%;
+  display: flex;
+  flex-direction: row;
 }
-.button_cancle{
-  height:40px;
-  line-height:40px;
+.scenic-spot-recommendation-view-inner {
+  width: 70px;
+  padding: 5px;
+  margin-left: 5px;
 }
-.dw-text-confirm{
-  width: 25%;
-  height:15%;
-  top: 60%;
-  position: absolute;
-  font-size: 12px;
-  white-space: normal;
-  left: 60%;
-  color: #777;
-  line-height: 1.5;
-}
-.button_confirm{
-  height:40px;
-  line-height:40px;
-  background-color: rgb(233, 76, 37);
-  color: #fff;
-}
-.liuyan {
-  width: 40px;
-  height: 40px;
-  position: absolute;
-  top: 9%;
-  /* bottom: 3%; */
-  right: 5%;
-  animation: twinkling 2s 3 ease-in-out;
-}
-.search_bar{
-  position: absolute;
-  width: 100%;
-  top: 0px;
-  height:45px;
+.scenic-spot-recommendation-view-inner-image {
+  height: 100%;
+  width: 70px;
+  border-radius: 15%;
 }
 </style>
