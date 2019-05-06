923b9c4a58042ffd9cdb6efcad40e6b4b778706c
Xin <md102@MD102deMacBook-Pro.local>
2019-05-06 22:29:42 +0800

    update signin

--- src/pages/center/signin/index.vue
+++ src/pages/center/signin/index.vue
@@ -1,115 +1,172 @@
 <template >
-  <view class="signin-page">
-    <!-- <wux-tabs theme="assertive" controlled :current="current" @change="onChange">
-        <wux-tab key="signin_tab1">
-            <wux-badge count="3">累计签到</wux-badge>
-        </wux-tab>
-    </wux-tabs> -->
-    <!-- <head-message :valueData="valueData"> </head-message> -->
-    <navigation-card :data="activityList" :iconClick="iconClick"/>
+  <view class="sight-page">
+    <view class="activity-sight-list">
+      <sight-card :data="sightList" :iconClick="iconClick"/>
+    </view>
   </view>
 </template>
 
 <script>
-import navigationCard from '../../../components/navigation-card';
-import headMessage from '../../../components/head-message';
+import sightCard from '../../../components/sight-card';
 export default {
   components: {
-    navigationCard,
-    headMessage
+    sightCard
   },
   data () {
     return {
-      current: 'signin_tab1',
-      valueData: '累计签到： 28次',
-      activityList: [
+      sightList: [
         {
           src: '../../static/images/fudan.jpg',
-          activityName: '复旦一日游',
-          iconType: 'ios-navigate',
-          iconColor: '#007fff',
-          iconText: '导航',
-          isIcon: true
+          sightName: '中共一大会址纪念馆',
+          iconType: 'ios-checkmark-circle',
+          sightStatus: '已完成',
+          iconColor: 'red',
+          iconArr: [
+            {
+              icon: 'ios-heart',
+              size: '16',
+              color: '#888',
+              label: 11
+            },
+            {
+              icon: 'ios-chatboxes',
+              size: '16',
+              color: '#888',
+              label: 11
+            }
+          ]
         },
         {
           src: '../../static/images/fudan.jpg',
-          activityName: '复旦一日游',
-          iconType: 'ios-navigate',
-          iconColor: '#007fff',
-          iconText: '导航',
-          isIcon: true
+          sightName: '中共一大会址纪念馆',
+          iconType: 'ios-checkmark-circle',
+          sightStatus: '已完成',
+          iconColor: 'red',
+          iconArr: [
+            {
+              icon: 'ios-heart',
+              size: '16',
+              color: '#888',
+              label: 11
+            },
+            {
+              icon: 'ios-chatboxes',
+              size: '16',
+              color: '#888',
+              label: 11
+            }
+          ]
         },
         {
           src: '../../static/images/fudan.jpg',
-          activityName: '复旦一日游',
-          iconType: 'ios-navigate',
-          iconColor: '#007fff',
-          iconText: '导航',
-          isIcon: true
+          sightName: '中共一大会址纪念馆',
+          iconType: 'ios-checkmark-circle',
+          sightStatus: '已完成',
+          iconColor: 'red',
+          iconArr: [
+            {
+              icon: 'ios-heart',
+              size: '16',
+              color: '#888',
+              label: 11
+            },
+            {
+              icon: 'ios-chatboxes',
+              size: '16',
+              color: '#888',
+              label: 11
+            }
+          ]
         },
         {
           src: '../../static/images/fudan.jpg',
-          activityName: '复旦一日游',
-          iconType: 'ios-navigate',
-          iconColor: '#007fff',
-          iconText: '导航',
-          isIcon: true
+          sightName: '中共一大会址纪念馆',
+          iconType: 'ios-checkmark-circle',
+          sightStatus: '已完成',
+          iconColor: 'red',
+          iconArr: [
+            {
+              icon: 'ios-heart',
+              size: '16',
+              color: '#888',
+              label: 11
+            },
+            {
+              icon: 'ios-chatboxes',
+              size: '16',
+              color: '#888',
+              label: 11
+            }
+          ]
         },
         {
           src: '../../static/images/fudan.jpg',
-          activityName: '复旦一日游',
-          iconType: 'ios-navigate',
-          iconColor: '#007fff',
-          iconText: '导航',
-          isIcon: true
-        },
-        {
-          src: '../../static/images/fudan.jpg',
-          activityName: '复旦一日游',
-          iconType: 'ios-navigate',
-          iconColor: '#007fff',
-          iconText: '导航',
-          isIcon: true
-        },
-        {
-          src: '../../static/images/fudan.jpg',
-          activityName: '复旦一日游',
-          iconType: 'ios-navigate',
-          iconColor: '#007fff',
-          iconText: '导航',
-          isIcon: true
-        },
-        {
-          src: '../../static/images/fudan.jpg',
-          activityName: '复旦一日游',
-          iconType: 'ios-navigate',
-          iconColor: '#007fff',
-          iconText: '导航',
-          isIcon: true
+          sightName: '中共一大会址纪念馆',
+          iconType: 'ios-checkmark-circle',
+          sightStatus: '已完成',
+          iconColor: 'red',
+          iconArr: [
+            {
+              icon: 'ios-heart',
+              size: '16',
+              color: '#888',
+              label: 11
+            },
+            {
+              icon: 'ios-chatboxes',
+              size: '16',
+              color: '#888',
+              label: 11
+            }
+          ]
         }
-
       ]
     };
   },
   methods: {
     iconClick (item) {
-      console.log('item', item)
-      wx.navigateTo({ url: 'activityDetail/main?name=' + item.activityName })
-    },
-    onChange (e) {
-      console.log('onChange', e)
-      this.current = e.target.key;
+      console.log('item', item);
+      // wx.navigateTo({ url: 'sightDetail/main?name=' + item.sightName })
     }
+  },
+  onShareAppMessage: function (ops) {
+    return {
+      title: '红色印记'
+    };
   }
 };
 </script>
 
 <style>
-.signin-page{
+.sight-page {
   background-color: #f4f4f4;
 }
-.signin-page .navigation-card-content-title {
-  padding-top: 25px;
-  padding-left: 5px;
+
+.background-image image {
+  width: 100%;
+}
+.activity-sight-title-group{
+  display: flex;
+  justify-content: space-between;
+}
+.activity-sight-title-share button {
+  font-size: 20px;
+  font-weight: 600;
+  margin-left: 5px;
+  line-height: 40px;
+  padding: 0;
+  border-radius: 0px;
+  box-sizing: inherit;
+  background-color: #fff;
+  display: flex;
+}
+.activity-sight-title-share button::after {
+  border: none;
+}
+.activity-sight-title-share .button-hover {
+  color: #000;
+}
+.activity-sight-title-label{
+  line-height: 40px;
 }
 </style>
---
+++ src/pages/center/signin/index_1.vue
@@ -0,0 +1,115 @@
+<template >
+  <view class="signin-page">
+    <!-- <wux-tabs theme="assertive" controlled :current="current" @change="onChange">
+        <wux-tab key="signin_tab1">
+            <wux-badge count="3">累计签到</wux-badge>
+        </wux-tab>
+    </wux-tabs> -->
+    <!-- <head-message :valueData="valueData"> </head-message> -->
+    <navigation-card :data="activityList" :iconClick="iconClick"/>
+  </view>
+</template>
+
+<script>
+import navigationCard from '../../../components/navigation-card';
+import headMessage from '../../../components/head-message';
+export default {
+  components: {
+    navigationCard,
+    headMessage
+  },
+  data () {
+    return {
+      current: 'signin_tab1',
+      valueData: '累计签到： 28次',
+      activityList: [
+        {
+          src: '../../static/images/fudan.jpg',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '导航',
+          isIcon: true
+        },
+        {
+          src: '../../static/images/fudan.jpg',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '导航',
+          isIcon: true
+        },
+        {
+          src: '../../static/images/fudan.jpg',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '导航',
+          isIcon: true
+        },
+        {
+          src: '../../static/images/fudan.jpg',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '导航',
+          isIcon: true
+        },
+        {
+          src: '../../static/images/fudan.jpg',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '导航',
+          isIcon: true
+        },
+        {
+          src: '../../static/images/fudan.jpg',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '导航',
+          isIcon: true
+        },
+        {
+          src: '../../static/images/fudan.jpg',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '导航',
+          isIcon: true
+        },
+        {
+          src: '../../static/images/fudan.jpg',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '导航',
+          isIcon: true
+        }
+
+      ]
+    };
+  },
+  methods: {
+    iconClick (item) {
+      console.log('item', item)
+      wx.navigateTo({ url: 'activityDetail/main?name=' + item.activityName })
+    },
+    onChange (e) {
+      console.log('onChange', e)
+      this.current = e.target.key;
+    }
+  }
+};
+</script>
+
+<style>
+.signin-page{
+  background-color: #f4f4f4;
+}
+.signin-page .navigation-card-content-title {
+  padding-top: 25px;
+  padding-left: 5px;
+}
+</style>
