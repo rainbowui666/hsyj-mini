f6df6d435d2395d915ba255da6d5fe1e4bd9affe
Xin <md102@MD102deMacBook-Pro.local>
2019-05-09 01:01:21 +0800

    update search

--- src/components/search-card.vue
+++ src/components/search-card.vue
@@ -1,11 +1,26 @@
 <template>
-  <view>
+  <view style="padding: 0 20px;">
     <!-- <navigator url="/pages/map/navigation/main?activitySight=true"> -->
       <view v-for="(item,index) in data" :key="index" class="search-card">
+        <image class="gpsImg" :src="item.gpsImg" @click="thumbsupClick(item,index)"/>
+        <view class="search-card-content">
+        <view class="search-card-content-title">
+            <view>{{ item.activityName }}</view>
+        </view>
+        <view class="search-card-content-desc">
+            <view class="search-card-content-desc-item">
+              <text>旅游景点</text>
+            </view>
+            <view class="search-card-content-desc-item">
+              <view>{{ item.address }}</view>
+            </view>
+        </view>
+        </view>
         <view class="search-card-icon">
-          <view v-if="item.isIcon" class="search-card-icon-inner" @click="click(item)">
+          <image class="navigationImg" :src="item.navigationImg" @click="thumbsupClick(item,index)"/>
+          <!-- <view v-if="item.isIcon" class="search-card-icon-inner" @click="click(item)">
             <wux-icon :type="item.iconType" :color="item.iconColor"/>
-          </view>
+          </view> -->
           <view class="search-card-icon-text">
             <view class="on-status">{{ item.iconText }}</view>
           </view>
@@ -45,10 +60,63 @@ export default {
 .search-card {
   display: flex;
   width: 100%;
-  height: 85px;
+  height: 75px;
   /* border-bottom:1px solid #888; */
   margin-bottom: 1px;
   background-color: #fff;
+  border-bottom: 1px solid #d4d4d4;
+}
+.search-card:last-child {
+  border-bottom: none;
+}
+.gpsImg{
+  height: 30px;
+  width: 30px;
+  margin:auto 10px;
+}
+.search-card-content{
+  /* display: flex; */
+  width: 65%;
+  overflow: hidden;
+}
+.search-card-content-title{
+  margin: 10px 5px;
+  font-size: 14px;
+  max-height: 58px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.search-card-content-desc {
+  display: flex;
+  /* flex-direction: column; */
+  padding: 5rpx 20rpx 10rpx 20rpx;
+  font-size: 26rpx;
+  color: #777;
+  /* white-space: nowrap; */
+  flex-grow: 1;
+  /* overflow:hidden;
+  text-overflow:ellipsis;
+  white-space:nowrap */
+}
+.search-card-content-desc-item {
+  display: flex;
+  flex-direction: row;
+  /* white-space: no-wrap; */
+  /* overflow:hidden;
+  text-overflow:ellipsis;
+  white-space:nowrap */
+}
+.search-card-content-desc-item ._text {
+  white-space: nowrap;
+  margin-right:10px;
+}
+.search-card-content-desc-item ._view {
+  height: 20px;
+  /* overflow: hidden; */
+  width: 70%;
+  overflow:hidden;
+  text-overflow:ellipsis;
+  white-space:nowrap
 }
 .search-card-icon {
   width: 15%;
@@ -63,5 +131,13 @@ export default {
 .search-card-icon-text {
   text-align: center;
   font-size: 13px;
+}
+.navigationImg{
+  height: 30px;
+  width: 30px;
+  position:relative;
+  left:50%;
+  transform:translateX(-50%);
+  /* transform:rotate(45deg); */
 }
 </style>
--- src/pages/map/navigation/index.vue
+++ src/pages/map/navigation/index.vue
@@ -1,5 +1,5 @@
 <template >
-  <view class="navigation-page">
+  <view class="search-page">
     <wux-search-bar />
     <search-card :data="activityList" :iconClick="iconClick"/>
   </view>
@@ -15,12 +15,64 @@ export default {
     return {
       activityList: [
         {
-          src: '../../../static/images/fudan.jpg',
+          gpsImg: '../../../static/images/gpsImg.png',
+          navigationImg: '../../../static/images/navigationImg.png',
           activityName: '复旦一日游',
           iconType: 'ios-navigate',
           iconColor: '#007fff',
-          iconText: '导航',
-          isIcon: true
+          iconText: '3.9km',
+          isIcon: true,
+          address: '上海市-黄浦区-中山东路666号-88栋'
+        },
+        {
+          gpsImg: '../../../static/images/gpsImg.png',
+          navigationImg: '../../../static/images/navigationImg.png',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '3.9km',
+          isIcon: true,
+          address: '上海市-黄浦区-中山东路'
+        },
+        {
+          gpsImg: '../../../static/images/gpsImg.png',
+          navigationImg: '../../../static/images/navigationImg.png',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '3.9km',
+          isIcon: true,
+          address: '上海市-黄浦区-中山东路'
+        },
+        {
+          gpsImg: '../../../static/images/gpsImg.png',
+          navigationImg: '../../../static/images/navigationImg.png',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '3.9km',
+          isIcon: true,
+          address: '上海市-黄浦区-中山东路'
+        },
+        {
+          gpsImg: '../../../static/images/gpsImg.png',
+          navigationImg: '../../../static/images/navigationImg.png',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '3.9km',
+          isIcon: true,
+          address: '上海市-黄浦区-中山东路'
+        },
+        {
+          gpsImg: '../../../static/images/gpsImg.png',
+          navigationImg: '../../../static/images/navigationImg.png',
+          activityName: '复旦一日游',
+          iconType: 'ios-navigate',
+          iconColor: '#007fff',
+          iconText: '3.9km',
+          isIcon: true,
+          address: '上海市-黄浦区-中山东路'
         }
       ]
     };
