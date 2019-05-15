8f595555c4932386a44ebeab00807d54a7139e54
Xin <md102@MD102deMacBook-Pro.local>
2019-05-15 22:41:12 +0800

    add login

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,21 +1,20 @@
 import request from './request'
 
 const ApiRootUrl = 'https://hsyj.100eduonline.com/api/'
-const ApiGateWayUrl = 'https://sit.ebaocloud.com/api/'
 
 const api = {
   // 获得服务列表
-  login: (paramObj) => request.post('user/login', paramObj, {
-    baseURL: ApiRootUrl
-  }),
+  // login: (paramObj) => request.post('user/login', paramObj, {
+  //   baseURL: ApiRootUrl
+  // }),
   // 发送验证码
-  getVerification: (paramObj) => request.post('two-factor-service/2fa/sendCode ', paramObj, {
-    baseURL: ApiGateWayUrl
-  }),
+  // getVerification: (paramObj) => request.post('two-factor-service/2fa/sendCode ', paramObj, {
+  //   baseURL: ApiGateWayUrl
+  // }),
   // 验证码检验
-  validateVerification: (paramObj) => request.post('two-factor-service/2fa/verifyCode', paramObj, {
-    baseURL: ApiGateWayUrl
-  }),
+  // validateVerification: (paramObj) => request.post('two-factor-service/2fa/verifyCode', paramObj, {
+  //   baseURL: ApiGateWayUrl
+  // }),
   // 首页获取推荐活动和景点
   getRecommendList: (r) => request.get('api/index/home', null, {
     baseURL: ApiRootUrl
@@ -37,6 +36,9 @@ const api = {
     baseURL: ApiRootUrl
   }),
   getSearchListBykeyWord: (obj) => request.get('api/search/searchList?pageindex=' + obj.pageindex + '&pagesize=' + obj.pagesize + '&keyword=' + obj.keyword, null, {
+    baseURL: ApiRootUrl
+  }),
+  indexLogin: (paramObj) => request.post('api/auth/login', paramObj, {
     baseURL: ApiRootUrl
   })
 }
