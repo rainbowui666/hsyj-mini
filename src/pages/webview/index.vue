9193e160d85e3431580459fdfb6b1b9f15036010
Danni <630082237@qq.com>
2019-05-30 22:44:35 +0800

    update

--- src/pages/webview/index.vue
+++ src/pages/webview/index.vue
@@ -4,23 +4,12 @@
 
 <script>
 export default {
-  // async onLoad (options) {
-  //   if (options.id) {
-  //     const item = wx.getStorageSync(options.id);
-  //     this.src = item.url;
-  //   } else if (options.result) {
-  //     const item = wx.getStorageSync(options.result);
-  //     this.src = item.url;
-  //   } else {
-  //     this.src = options.url;
-  //   }
-  // },
   onShow () {
-    this.src = this.$mp ? this.$mp.query.videoUrl ? this.$mp.query.videoUrl : '' : ''
+    this.src = this.$mp ? this.$mp.query.videoUrl ? 'https://hsyj.100eduonline.com/static/mini-images/index.html?url=' + this.$mp.query.videoUrl : '' : ''
   },
   data () {
     return {
-      src: this.$mp ? this.$mp.query.videoUrl ? this.$mp.query.videoUrl : '' : ''
+      src: this.$mp ? this.$mp.query.videoUrl ? 'https://hsyj.100eduonline.com/static/mini-images/index.html?url=' + this.$mp.query.videoUrl : '' : ''
     };
   }
 };
